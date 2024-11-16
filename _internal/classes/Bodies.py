e647bf23b76b819a1279fb4ae70bc1034897553b
VitoR-InA <85871524+VitoR-InA@users.noreply.github.com>
2024-11-16 14:33:12 +0500

    Update Bodies.py

--- _internal/classes/Bodies.py
+++ _internal/classes/Bodies.py
@@ -47,16 +47,35 @@ class Box:
 class Player:
     def __init__(self,
                  rect: pg.Rect,
-                 color: Tuple[int, int, int, int],
+                 health: int,
                  space: pm.Space):
+        #Player rect
         self.rect = rect
+        
+        #Player health
+        self.health = health
+        self.colors = []
+        for heart in range(1, self.health + 1):
+            factor = heart / health
+            self.colors.append((int(255 * factor), int(255 * (1 - factor)), 0, 255))
+        self.colors.append((7, 7, 7, 255))
+        self.colors.reverse()
+        
+        #Player body
         self.body = pm.Body(body_type=pm.Body.KINEMATIC)
         self.body.position = rect.topleft
+        
+        #Player shape
         self.shape = pm.Poly.create_box(self.body, self.rect.size)
         self.shape.elasticity = 1
         self.shape.friction = 0
-        self.shape.color = color
+        
+        #Add player to space
         space.add(self.body, self.shape)
 
     def draw(self, surface):
+        #Change color
+        self.shape.color = self.colors[self.health]
+        
+        #Draw player
         pg.draw.rect(surface, self.shape.color, pg.Rect((self.body.position[0] - self.rect.size[0] / 2, self.body.position[1] - self.rect.size[1] / 2), self.rect.size))
