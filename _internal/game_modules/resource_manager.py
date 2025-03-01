b2545003370c7862c98e4b6bf309da4bdee8350b
VitoR-InA <85871524+VitoR-InA@users.noreply.github.com>
2025-03-01 13:25:15 +0500

    Update resource_manager.py

--- _internal/game_modules/resource_manager.py
+++ _internal/game_modules/resource_manager.py
@@ -1,3 +1,5 @@
+from configobj import ConfigObj
+
 import os
 import shutil
 
@@ -6,22 +8,25 @@ from zipfile import ZipFile
 
 class ResourceManager:
     "Simple tool to work with PingPY resources"
-    @classmethod
+    def __init__(self, resources_path: os.PathLike):
+        self.resources_path = resources_path
+        self.config_obj = ConfigObj(os.path.join(self.resources_path, ".properties"))
+        if not self.config_obj.get("chosen_resource"):
+            self.config_obj["chosen_resource"] = "default.zip"
+        self.config_obj.write()
+
     def load(self, file: os.PathLike):
         with ZipFile(file) as zip:
-            self.path = os.path.join(os.path.dirname(zip.filename), f".{os.path.splitext(os.path.basename(zip.filename))[0]}")
-            zip.extractall(self.path)
-            if os.name == "nt": os.system(f"attrib +H {self.path}")
+            self.resource = os.path.join(self.resources_path, f".{os.path.splitext(os.path.basename(zip.filename))[0]}")
+            zip.extractall(self.resource)
+            if os.name == "nt": os.system(f"attrib +H {self.resource}")
 
-    @classmethod
     def has(self, path: os.PathLike) -> bool:
-        if os.path.exists(os.path.join(self.path, path)):
+        if os.path.exists(os.path.join(self.resource, path)):
             return True
         else: return False
 
-    @classmethod
     def get(self, path: os.PathLike) -> os.PathLike:
-        return os.path.join(self.path, path)
+        return os.path.join(self.resource, path)
 
-    @classmethod
-    def close(self): shutil.rmtree(self.path)
+    def close(self): shutil.rmtree(self.resource)
